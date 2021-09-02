2cc4f3d408009f4fcd8d521a93d175e33ed4e0e0
qianiaoo <adnimm@163.com>
2021-09-02 15:12:14 +0900

    new login

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="fillcontain">
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  name: 'app',
+  components: {
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
+
+</script>
+
+<style lang="less">
+@import './style/common';
+
 </style>
