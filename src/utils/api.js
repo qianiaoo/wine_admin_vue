af5c1a566966111523f7bcb13e123e46534af3a7
qianiaoo <adnimm@163.com>
2021-09-09 01:56:19 +0900

    新增标签

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,7 @@ import http from '../utils/http'
 
 export const baseImgPath = ''
 export const baseUrl = 'http://localhost:8010'
+
 export const login = data => http.post('/user/login', data);
 
 export const logout = () => http.post('/user/logout');
